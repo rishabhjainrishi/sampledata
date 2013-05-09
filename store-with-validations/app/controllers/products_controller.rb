f95bcd2a21dfa3e2b28746db5a2a42a7ee4bc965
Rishabh Jain <rjain@kou.pn>
2013-05-09 12:48:10 -0500

    Sortable Function Added

--- store-with-validations/app/controllers/products_controller.rb
+++ store-with-validations/app/controllers/products_controller.rb
@@ -1,10 +1,20 @@
 class ProductsController < ApplicationController
+  helper_method :sort_column, :sort_direction
+  
   def index
-    @products = Product.order(:name)
+    @products = Product.order(sort_column + " " + sort_direction)
     respond_to do |format|
       format.html
       format.csv { send_data @products.to_csv }
       format.xls # { send_data @products.to_csv(col_sep: "\t") }
     end
+  end
+  
+  def sort_column
+    Product.column_names.include?(params[:sort]) ? params[:sort] : "name"
+  end
+  
+  def sort_direction
+    %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
   end
 end
